c5e1607c84781cba4eab8be08b1225435c1d2b4d
ABM988989 <62875424+ABM988989@users.noreply.github.com>
2021-03-13 23:26:16 +0200

    initial

---
+++ tooltip-wiki.js
@@ -0,0 +1,105 @@
+// Get all the links that are in the DIV of the post.
+
+const classPost = 'entry-content'; // Change it, to the class of the element of the post content.
+
+let links = document.getElementsByClassName(classPost)[0].getElementsByTagName('a');
+
+
+// Loop through all the links, and add a mouse-event to them.
+for (const link of links){
+    link.addEventListener('mouseenter', showTooltip);
+}
+
+// Set variable for setTimeout. 
+let delayTimer;
+
+
+// Show Tooltip. 
+function showTooltip(event) {
+
+
+    // Post URL.
+    let post_url = event.target.getAttribute('href');
+
+    // Check if this URL is Internal link.
+    let domain_link = new URL(post_url);
+    domain_link = domain_link.hostname;
+    if ( window.location.hostname == domain_link) {
+        console.log('YES, is Internal link!')
+    } else {
+        console.log('Sorry, is external link!')
+        return;
+    }
+
+    // Cancel for all other setTimeout.
+    clearTimeout(delayTimer);
+
+    // Remove all existing Tooltips.
+    document.querySelectorAll('.tooltip-container').forEach( (tooltips) => tooltips.remove() );
+
+
+    delayTimer = setTimeout(() => {
+
+
+        let tooltipContainer = document.createElement('DIV');
+        tooltipContainer.classList.add('tooltip-container');
+    
+        tooltipContainer.style.top = (event.pageY - 20 ) + 'px';
+        tooltipContainer.style.left = (event.pageX - 20 ) + 'px';
+    
+        // TEXT
+        let tooltipText = document.createElement("A");
+        tooltipText.classList.add('tooltip-text');
+        tooltipContainer.appendChild(tooltipText);
+    
+        document.getElementsByTagName('body')[0].appendChild(tooltipContainer);
+
+
+        // AJAX Request.
+        const data = new FormData();
+        data.append( 'action', 'tooltip_action' );
+        data.append( 'post_url', post_url );
+        data.append( 'nonce', readmelater_ajax.nonce );
+        
+        fetch(readmelater_ajax.ajax_url, {
+            method: "POST",
+            credentials: 'same-origin',
+            body: data
+        })
+        .then((response) => response.json())
+        .then((response) => {
+
+            tooltipText.setAttribute('href', response.data.post_parmalink);
+
+            // Image
+            let tooltipImg = document.createElement("IMG");
+            tooltipImg.classList.add('tooltip-img');
+            tooltipImg.setAttribute('src', response.data.post_img);
+            tooltipText.appendChild(tooltipImg);
+
+            // Title
+            let tooltipTitle = document.createElement("H3");
+            tooltipTitle.classList.add('tooltip-title');
+            let titlenode = document.createTextNode(response.data.post_title);
+            tooltipTitle.appendChild(titlenode);
+            tooltipText.appendChild(tooltipTitle);     
+
+            // node text
+            let textnode = document.createTextNode(response.data.post_contect);
+            tooltipText.appendChild(textnode);
+        
+
+            // Add Event for Remove Tooltip on mouse leave tooltip.
+            tooltipContainer.addEventListener('mouseleave',(e) => { 
+                e.target.remove()
+            });
+    
+            
+        })
+        .catch((error) => {
+        console.error(error);
+        });          
+
+    }, 1000);
+    
+}
---
+++ tooltip-wiki.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Tooltip Wiki - ABM
+Version: 1.0.0
+*/
+
+class Tooltip_Wiki {
+
+    public function __construct() {
+
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+        add_action( 'wp_ajax_nopriv_tooltip_action', array( $this, 'ajax_action' ) );
+        add_action( 'wp_ajax_tooltip_action', array( $this, 'ajax_action' ) );
+
+    }
+
+    public function enqueue_scripts() {
+        wp_register_script( 'tooltip-ajax-script', plugins_url( 'tooltip-wiki.js', __FILE__ ), array(), false, true );
+        wp_localize_script( 'tooltip-ajax-script', 'readmelater_ajax', array(
+            'ajax_url' => admin_url( 'admin-ajax.php' ),
+            'nonce' => wp_create_nonce( 'adielnonce' )
+          ) );
+
+        wp_enqueue_script( 'tooltip-ajax-script' );
+
+        wp_enqueue_style( 'tooltip-wiki-style', plugins_url( 'tooltip-wiki.css', __FILE__ ) );
+    
+     } 
+
+
+
+     public function ajax_action() {
+        
+
+        // Check wp_create_nonce:
+        check_ajax_referer( 'adielnonce', 'nonce' );
+
+
+        // Get Post URL:
+        $post_url = $_POST['post_url'];
+
+        $post_url_path = parse_url($post_url)['path'];
+
+        $post_url_path = str_replace('/', '', $post_url_path);
+        $post_url_path = str_replace('wordpress', '', $post_url_path);
+
+
+
+
+        // Get Post OBJECT Using Slug:
+        $post = get_page_by_path($post_url_path, OBJECT, 'post');
+
+        // Get Post Contect Using Post OBJECT:
+        $post_contect = get_the_content(null, false, $post);
+        $post_title = get_the_title($post);
+        $post_parmalink = get_the_permalink($post);
+        $post_img = get_the_post_thumbnail_url($post);
+
+
+        /*   ======= AJAX OUTPUT ======= */ 
+
+        $return = array(
+            'post_contect' => mb_strimwidth($post_contect, 0, 200),
+            'post_title' => $post_title,
+            'post_img' => $post_img,
+            'post_parmalink' => $post_parmalink
+
+        );
+        wp_send_json_success( $return );
+
+
+    } // end function ajax_action()
+
+
+}
+
+new Tooltip_Wiki;
